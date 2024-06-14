424f44b3683f801ceda6c11be6fbc77a55c4c510
walltch <walltch@github.com>
2024-06-14 09:21:46 +0200

    update pokemon card

--- src/components/pokemon/PokemonCard.jsx
+++ src/components/pokemon/PokemonCard.jsx
@@ -1,20 +1,23 @@
 import getTypeColor from "./getTypeColor";
-export default function PokemonCard( {pokemonData, visibleCount}) {
-    return(
-      <>
-        <h2 className="text-4xl text-center p-6 font-bold tracking-tight text-primary-yellow">Tous les Pokemons</h2>
-        <ul 
-          className="grid grid-cols-5 gap-4 mt-10">
-          {pokemonData.slice(0, visibleCount).map((pokeman) => (
-            <li
-              key={pokeman.id}
-              style={{backgroundColor: getTypeColor(pokeman.type),}}
-              className= "card relative aspect-square	w-full my-8">
-              <img
-                className="card-image h-44 aspect-square object-cover mx-auto absolute left-1/2 transform -translate-x-1/2 -translate-y-1/2 "
-                src={pokeman.image}
-                alt={pokeman.name}
-              />
+export default function PokemonCard({ pokemonData, visibleCount }) {
+  return (
+    <div className="container">
+      <h2 className="text-4xl text-center p-6 font-bold tracking-tight text-primary-yellow">
+        Tous les Pokemons
+      </h2>
+      <ul className="mx-10 sm:grid sm:grid-cols-5 sm:gap-4">
+        {pokemonData.slice(0, visibleCount).map((pokeman) => (
+          <li
+            key={pokeman.id}
+            style={{ backgroundColor: getTypeColor(pokeman.type) }}
+            className="card my-10"
+          >
+            <img
+              className="card-image sm:h-44 h-60 aspect-square object-cover mx-auto"
+              src={pokeman.image}
+              alt={pokeman.name}
+            />
+            <div className="card-body text-left pl-3 pb-3 ">
               <h2 className="card-title">
                 {pokeman.id}. {pokeman.name}
               </h2>
@@ -22,9 +25,10 @@ export default function PokemonCard( {pokemonData, visibleCount}) {
               <p className="card-subtitle">Height: {pokeman.height}</p>
               <p className="card-subtitle">Weight: {pokeman.weight}</p>
               <p className="card-subtitle">Abilities: {pokeman.abilities}</p>
-            </li>
-          ))}
-        </ul>
-      </>
-    )
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
